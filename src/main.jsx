5cb92049c949fee995c4af017af7fd43a4e7d7cc
Yolanda <ylopez.tech@gmail.com>
2025-03-08 14:24:50 +0100

    Renombre GifExpertApp

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {GifExpertApp} from './GitExpertApp'
+import {GifExpertApp} from './GifExpertApp'
 import './styles.css'
 
 createRoot(document.getElementById('root')).render(
